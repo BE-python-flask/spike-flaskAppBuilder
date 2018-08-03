df05c1d2f72e9c7d0dddcef7d3eb7216d8e43475
rockwang <rockwang@localhost.localdomain>
2018-08-03 20:51:03 +0800

    5.7 Flask-AppBuilder实现学校后台管理

--- app/views.py
+++ app/views.py
@@ -1,26 +1,38 @@
-from flask import render_template
 from flask_appbuilder.models.sqla.interface import SQLAInterface
-from flask_appbuilder import ModelView
 from app import appbuilder, db
 from flask_appbuilder import has_access
 
 from .models import ContactGroup, Contact
 
+
+from flask import render_template, flash
+from flask.ext.appbuilder import ModelView, MultipleView, MasterDetailView
+from flask_appbuilder import AppBuilder, expose, BaseView, has_access, SimpleFormView
+from flask_babel import lazy_gettext as _
+from flask_appbuilder.charts.views import DirectByChartView
+
+from wtforms import Form, StringField
+from wtforms.validators import DataRequired
+from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
+from flask_appbuilder.forms import DynamicForm
+
+from flask_appbuilder.widgets import ListThumbnail
+from .models import College, Department, Major, MClass, Teacher, Student
+from flask_appbuilder.actions import action
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from flask_appbuilder.fieldwidgets import BS3TextFieldWidget, Select2Widget
+
 """
     Create your Views::
-
 
     class MyModelView(ModelView):
         datamodel = SQLAInterface(MyModel)
 
-
     Next, register your Views::
-
 
     appbuilder.add_view(MyModelView, "My View", icon="fa-folder-open-o", category="My Category", category_icon='fa-envelope')
 """
-from flask_appbuilder import AppBuilder, expose, BaseView
- 
+
 class MyView(BaseView):
     # route_base = "/myview"
     default_view = 'hello'
@@ -28,7 +40,11 @@ class MyView(BaseView):
     @expose('/hello/')
     def hello(self):
         return 'Hello World!'
- 
+
+from flask_appbuilder import IndexView
+
+class MyIndexView(IndexView):
+    index_template = 'index.html'
     @expose('/message/<string:msg>')
     @has_access
     def message(self, msg):
@@ -39,7 +55,7 @@ class MyView(BaseView):
     @has_access
     def welcome(self, msg):
         msg = 'Hello %s' % (msg)
-        return self.render_template('index.html',msg = msg)
+        return self.render_template('index.html', msg = msg)
 
 # appbuilder.add_view_no_menu(MyView())
 appbuilder.add_view(MyView, "Hello", category='My View')
@@ -63,8 +79,9 @@ class ContactModelView(ModelView):
     show_fieldsets = [
         (
             'Summary',
-            {'fields':
-                 ['name', 'address', 'contact_group']}
+            {
+                'fields': ['name', 'address', 'contact_group']
+             }
         ),
         (
             'Personal Info',
@@ -91,3 +108,37 @@ appbuilder.add_view(ContactModelView,
                     "List Contacts",
                     icon = "fa-address-card-o",
                     category = "Contacts")
+
+"""
+
+"""
+class CollegeView(ModelView):
+    datamodel = SQLAInterface(College)
+
+class DepartmentView(ModelView):
+    datamodel = SQLAInterface(Department)
+
+class MajorView(ModelView):
+    datamodel = SQLAInterface(Major)
+
+class MClassView(ModelView):
+    datamodel = SQLAInterface(MClass)
+
+class TeacherView(ModelView):
+    datamodel = SQLAInterface(Teacher)
+
+class StudentView(ModelView):
+    datamodel = SQLAInterface(Student)
+
+db.create_all()
+
+from flask_appbuilder import IndexView
+
+class MyIndexView(IndexView):
+    index_template = 'index.html'
+appbuilder.add_view(CollegeView, "College", icon="gear", category='School Manage',)
+appbuilder.add_view(DepartmentView, "Department", icon="gear",category='School Manage')
+appbuilder.add_view(MajorView, "Major", icon="gear", category='School Manage')
+appbuilder.add_view(MClassView, "MClass", icon="gear", category='School Manage')
+appbuilder.add_view(TeacherView, "Teacher", icon="gear",category='School Manage')
+appbuilder.add_view(StudentView, "Student", icon="gear",category='School Manage')
