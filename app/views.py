4ada3081cb70d91d7eafdd0587cea5f012be8348
rockwang <rockwang@localhost.localdomain>
2018-08-03 19:22:39 +0800

    5.6 模型视图

--- app/views.py
+++ app/views.py
@@ -4,6 +4,8 @@ from flask_appbuilder import ModelView
 from app import appbuilder, db
 from flask_appbuilder import has_access
 
+from .models import ContactGroup, Contact
+
 """
     Create your Views::
 
@@ -32,6 +34,7 @@ class MyView(BaseView):
     def message(self, msg):
         msg = 'Hello %s' % (msg)
         return msg
+
     @expose('/welcome/<string:msg>')
     @has_access
     def welcome(self, msg):
@@ -51,4 +54,40 @@ def page_not_found(e):
 
 db.create_all()
 
+class ContactModelView(ModelView):
+    datamodel = SQLAInterface(Contact)
 
+    label_columns = {'contact_group': 'Contacts Group'}
+    list_columns = ['name', 'personal_cellphone', 'birthday', 'contact_group']
+
+    show_fieldsets = [
+        (
+            'Summary',
+            {'fields':
+                 ['name', 'address', 'contact_group']}
+        ),
+        (
+            'Personal Info',
+            {'fields':
+                 ['birthday', 'personal_phone', 'personal_cellphone'],
+             'expanded':False
+             }
+        )
+    ]
+
+
+class GroupModelView(ModelView):
+    datamodel = SQLAInterface(ContactGroup)
+    related_views = [ContactModelView]
+
+db.create_all()
+
+appbuilder.add_view(GroupModelView,
+                    "List Groups",
+                    icon = "fa-address-book-o",
+                    category = "Contacts",
+                    category_icon = "fa-envelope")
+appbuilder.add_view(ContactModelView,
+                    "List Contacts",
+                    icon = "fa-address-card-o",
+                    category = "Contacts")
